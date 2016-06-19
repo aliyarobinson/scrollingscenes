775727fb5bc394c7d3c8dcc847113f71ca0b1526
aliya robinson <aliya robinson>
2016-06-18 21:50:31 -0400

    basic info window working

--- js/map.js
+++ js/map.js
@@ -96,15 +96,30 @@ var mapApp = mapApp || {};
 			mapApp.removeFilms();
 
 			for (var film in films){
-				console.log('map: ', map);
 				var latlng = new google.maps.LatLng(films[film].lat, films[film].lng);
 				map.setZoom(14);
+
 				marker = new google.maps.Marker({
 				  position: latlng,
 				  map: map
 				});
+
 				mapApp.currMarkers.push(marker);
 				map.setCenter(mapApp.mainLatLng);
+
+				(function(marker, film){
+
+					var contentString = '<div><p class="infoItem">Title: '+film.title+'</p></div>';
+
+	                var infowindow = new google.maps.InfoWindow({
+						content: contentString
+					});
+
+					marker.addListener('click', function() {
+						infowindow.open(map, marker);
+					});
+
+				})(marker, films[film])
 			}
 
 		},
@@ -113,160 +128,22 @@ var mapApp = mapApp || {};
 			for (var i = 0; i < mapApp.currMarkers.length; i++) {
 			    mapApp.currMarkers[i].setMap(null);
 			  }
+		},
+
+		mapRecenter: function(latlng,offsetx,offsety) {
+			var point1 = map.getProjection().fromLatLngToPoint(
+				(latlng instanceof google.maps.LatLng) ? latlng : map.getCenter()
+			);
+			var point2 = new google.maps.Point(
+				( (typeof(offsetx) == 'number' ? offsetx : 0) / Math.pow(2, map.getZoom()) ) || 0,
+				( (typeof(offsety) == 'number' ? offsety : 0) / Math.pow(2, map.getZoom()) ) || 0
+			);  
+			map.setCenter(map.getProjection().fromPointToLatLng(new google.maps.Point(
+				point1.x - point2.x,
+				point1.y + point2.y
+			)));
 		}
-	}
-
-	// function initialize() {
-	// 	geocoder = new google.maps.Geocoder();
-	// 	var latlng = new google.maps.LatLng(40.730885, -73.997383);
-	// 	var latlng_s1 = new google.maps.LatLng(40.682330346391097, -74.035749435424805);
-	// 	var mapOptions = {
-	// 	zoom: 13,
-	// 	scrollwheel: false,
-	// 	center: latlng,
-	// 	mapTypeId: google.maps.MapTypeId.ROADMAP
-	// 	};
-
-	// 	var directionsService = new google.maps.DirectionsService;
-	// 		var directionsDisplay = new google.maps.DirectionsRenderer;
-	// 	map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-	// 	directionsDisplay.setMap(map);
-
-	// 	$(document).on('click', '.directions', function(){
-	// 		console.log('clicked for directions');
-	// 		var lat = $(this).attr('data-lat');
-	// 		var lng = $(this).attr('data-lng');
-	// 		calcRoute(lat, lng);
-	// 	});
-
-	// 	$(document).on('click', '.decades button', function(){
-	// 		var decade = $(this).attr('data-decade');
-	// 		console.log('clicked for decadeBtn: ', decade);
-	// 	});
-
-	// 	function getFilms(){
-	// 		// Get Films.xml
-	// 		$.ajax({
-	// 		  type: "GET",
-	// 		  url: "Films.xml",
-	// 		  dataType: "xml",
-	// 		  success: function(xml) {
-	// 			// List film names in DOM
-	// 			$(xml).find('Film').each(function(){
-	// 			  var titleStr = $(this).find('Cell').eq(0).text();
-	// 			  var title = $.trim(titleStr);
-	// 			  var filmYearStr = $(this).find('Cell').eq(1).text();
-	// 			  var filmYear = $.trim(filmYearStr);
-	// 			  var lngStr = $(this).find('Cell').eq(10).text();
-	// 			  var lng = $.trim(lngStr);
-	// 			  var latStr = $(this).find('Cell').eq(9).text();
-	// 			  var lat = $.trim(latStr);
-	// 			  var filmBoroughStr = $(this).find('Cell').eq(11).text();
-	// 			  var filmBorough = $.trim(filmBoroughStr);	
-				  
-	// 			  filmsArr.push({'title':title, 'year': filmYear, 'lng': lng, 'lat': lat, 'borough':filmBorough});
-	// 			  // console.log('filmsArr: ', filmsArr);
-				  
-	// 			});
-	// 		  },
-	// 		  error : function(){console.log('error in parsing film info.');}	  
-	// 		});// End Get Films.xml
-		   
-	// 	}
-	// 	getFilms();
-
-
-	// 	var marker;
-	// 	var infowindow;
-	// 	var contentString;
-
-	// 	for (marker in markersArray){
-
-	// 		var thisLat = markersArray[marker].lat;
-	// 		var thisLng = markersArray[marker].lng;
-	// 		var thisLoc = new google.maps.LatLng(thisLat, thisLng);
-
-	// 		contentString = '<div id="content">'+
-	// 		'<div id="siteNotice">'+
-	// 		'</div>'+
-	// 		'<h1 id="firstHeading" class="firstHeading">'+ markersArray[marker].title +'</h1>'+
-	// 		'<button class="directions" data-lat="'+ thisLat +'" data-lng="'+ thisLng +'">Get Directions</button>'
-	// 		'</div>';
-
-	// 		var infowindow = new google.maps.InfoWindow({
-	// 			content: contentString
-	// 		});
-
-	// 		marker = new google.maps.Marker({
-	// 			position: thisLoc,
-	// 			map: map
-	// 		});
-
-			
-
-	// 		marker.addListener('click', function() {
-	// 			infowindow.open(map, marker);
-	// 		});
-	// 	}
-
-		
-	// 	function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-	// 		directionsService.route({
-	// 			origin: document.getElementById('start').value,
-	// 			destination: latlng_s1,
-	// 			travelMode: google.maps.TravelMode.DRIVING
-	// 		}, function(response, status) {
-	// 			if (status === google.maps.DirectionsStatus.OK) {
-	// 			  directionsDisplay.setDirections(response);
-	// 			} else {
-	// 			  window.alert('Directions request failed due to ' + status);
-	// 			}
-	// 		});
-	// 	}
-
-	// 	function calcRoute(lat, lng) {			    
-	// 		$this = $(this);
-	// 		if(navigator.geolocation) {
-	// 		  navigator.geolocation.getCurrentPosition(function(position) {
-	// 			var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);		
-													
-	// 			//get and set movie location
-	// 			var lat = lat;
-	// 			var lng = lng;
-	// 			// var movieLoc = new google.maps.LatLng(40.682330, -74.035749);					
-	// 			var movieLoc = new google.maps.LatLng(40.730885, -73.997383);					
-				
-	// 			var start = pos;
-	// 			var end = movieLoc;
-	// 			var request = {
-	// 				origin:start,
-	// 				destination: end,
-	// 				travelMode: google.maps.DirectionsTravelMode.DRIVING
-	// 			};
-
-	// 				  console.log('movieLoc: ', movieLoc);
-
-	// 			map.setCenter(pos);
-	// 			directionsService.route(request, function(response, status) {
-	// 			  if (status == google.maps.DirectionsStatus.OK) {
-	// 				  console.log('directions okay');
-	// 				directionsDisplay.setDirections(response);
-	// 			  }	else {
-	// 			  	console.log('dir not okay');
-	// 			  }			
-	// 			});
-	// 		  }, function() {
-	// 			handleNoGeolocation(true);
-	// 		  });
-	// 		} else {
-	// 		  // Browser doesn't support Geolocation
-	// 		  handleNoGeolocation(false);
-	// 		}
-	// 	}
-
-	// 	// Set Map Style
-	// 	map.setOptions({styles: styleDark}); 
-	// }
+	} // end mapApp
 
 })(jQuery);
 
